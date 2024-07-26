0c6bb114ad59aca5f9ecdd5d7143be635d7c7362
Ruslan Mirasov <info@mirasov.dev>
2024-07-26 21:55:31 +0200

    update sessions logic

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -11,28 +11,7 @@ const getAll = async (req, res) => {
     throw HttpError(404);
   }
 
-  const overall = result.length;
-
-  // Подсчет сессий со статусом 'done'
-  const success = result.filter(session => session.status === 'done').length || 0;
-
-  // Подсчет процента успешных сессий
-  const rate = `${((success / result.length) * 100).toFixed(1)}%` || '0%';
-
-  // Вычисление среднего значения длительности
-  const totalDuration = result.reduce((sum, session) => sum + session.duration, 0);
-  const averageDuration = totalDuration / result.length;
-  const minutes = Math.floor(averageDuration / 60000);
-  const seconds = Math.floor((averageDuration % 60000) / 1000);
-  const time = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}` || '00:00';
-
-  res.json({
-    overall,
-    success,
-    rate,
-    time,
-    collection: result,
-  });
+  res.json(result);
 };
 
 // GET SESSION BY ID
